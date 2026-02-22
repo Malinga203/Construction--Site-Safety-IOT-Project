2dc5a2cdea28fe473ec9566b2a1aaafb74daec2a
NazeefRizna <nazeefrizna@gmail.com>
2026-02-22 13:10:41 +0530

    Connecting Microcontroller to WiFi

--- IoT-Site-Safety-Vest/IoT-Site-Safety-Vest.ino
+++ IoT-Site-Safety-Vest/IoT-Site-Safety-Vest.ino
@@ -1,6 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
 
+const char* ssid = "";
+const char* password = "";
+
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  Serial.println("\nConnecting to WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+  Serial.println("Web server started");
 }
 
 void loop() {
